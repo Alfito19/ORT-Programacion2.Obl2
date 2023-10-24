58133f8054efce18773f7626e85b4949cd5bdde7
Juacin25 <joaquinhernande45@gmail.com>
2023-10-24 08:27:24 -0300

    agregamos la ventana de agregar tematica cuando se registra postulante

--- obligatorio2/src/obligatorio2/Obligatorio2.java
+++ obligatorio2/src/obligatorio2/Obligatorio2.java
@@ -7,6 +7,7 @@ public class Obligatorio2 {
     public static void main(String[] args) {
         Sistema controlador = new Sistema();
         Todo ventanas = new Todo(controlador);
+        //hay que cambiar la clase todo y que solo habra menú, y desde ahí inicializar las otras ventanas
     }
     
 }
