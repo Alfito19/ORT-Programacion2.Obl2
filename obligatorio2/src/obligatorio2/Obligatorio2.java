f8856182ebcf6439f991ea569275ee67993b8440
Juacin25 <joaquinhernande45@gmail.com>
2023-10-24 09:15:49 -0300

    saque todo

--- obligatorio2/src/obligatorio2/Obligatorio2.java
+++ obligatorio2/src/obligatorio2/Obligatorio2.java
@@ -6,7 +6,6 @@ public class Obligatorio2 {
 
     public static void main(String[] args) {
         Sistema controlador = new Sistema();
-        Todo ventanas = new Todo(controlador);
         //hay que cambiar la clase todo y que solo habra menú, y desde ahí inicializar las otras ventanas
         Menu vent = new Menu(controlador);
         vent.setVisible(true);
